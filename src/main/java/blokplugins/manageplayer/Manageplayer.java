c1b2e4561c8d6c44f384a0e3b178bfb2aa04ee36
blokongithub <blokongithub@gmail.com>
2024-06-13 18:45:23 +0100

    a

---
+++ src/main/java/blokplugins/manageplayer/Manageplayer.java
@@ -0,0 +1,17 @@
+package blokplugins.manageplayer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Manageplayer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
